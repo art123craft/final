298de1e304f12ce27f440bec16b118414c29a50f
EC2 Default User <ec2-user@ip-172-31-46-234.us-east-2.compute.internal>
2018-08-03 16:13:41 +0000

    first

--- application-controller.rb
+++ application-controller.rb
@@ -7,5 +7,51 @@ class MyApp < Sinatra::Base
  get'/' do
   erb :index
  end
+ 
+ 
+ post  '/results' do
+   answers = params.values
+   @total= 0
+   answers.each do |answer|
+     @total += answer.to_i
+   end
+   puts @total
+
+   @boy = boyquiz(@total)
+   if @boy == "nick"
+     erb :nick
+   elsif @boy == "zac"
+     erb :zac
+   elsif @boy == "shawn"
+     erb :shawn
+    elsif @boy == "chris"
+    erb :chris
+    elsif @boy == "tom"
+    erb :tom
+   end
+end
+post  '/res' do
+   answers = params.values
+   @total= 0
+   answers.each do |answer|
+     @total += answer.to_i
+   end
+   puts @total
+   
+   
+@girl = girlquiz(@total)
+   if @girl == "zendaya"
+     erb :zendaya
+   elsif @girl == "beyonce"
+     erb :beyonce
+   elsif @girl == "chrissy"
+     erb :chrissy
+    elsif @girl == "karlie"
+    erb :karlie
+    elsif @girl == "taylor"
+    erb :taylor
+   end
+end
 
 end
+
--- models/questions.rb
+++ models/questions.rb
@@ -1,1 +1,29 @@
 # This is where your Ruby code goes
+
+def boyquiz(input)
+    if input < 8
+        "nick"
+    elsif input < 15
+        "zac"
+    elsif input <22
+    "shawn"
+    elsif input <29
+    "chris"
+    elsif input <36
+    "tom"
+    end 
+end
+
+def girlquiz(input)
+    if input < 8
+        "zendaya"
+    elsif input < 15
+        "beyonce"
+    elsif input <22
+    "chrissy"
+    elsif input <29
+    "karlie"
+    elsif input <36
+    "taylor"
+    end 
+end
